a94dd4d58dffd01a3fae4011fe8dd437a2b277bf
jasminechin12 <jasminechin0412@gmail.com>
2018-10-02 17:01:53 -0400

    Lab 1

---
+++ Lab1/src/Collatz.java
@@ -0,0 +1,23 @@
+// Jasmine Chin 111717723
+
+import java.util.Scanner;
+
+public class Collatz {
+	
+	public static int howManyCollatzIterations(int n) {
+		if (n == 0 || n == 1)
+			return 0;
+		else if (n % 2 == 0)
+			return 1 + howManyCollatzIterations(n / 2);
+		else 
+			return 1 + howManyCollatzIterations(n*3 + 1);
+	}
+
+	public static void main(String[] args) {
+		System.out.print("Enter an integer: ");
+		Scanner input = new Scanner(System.in);
+		int n = input.nextInt();
+		System.out.println("Number of iterations returned by the method is: " + howManyCollatzIterations(n));
+	}
+
+}
